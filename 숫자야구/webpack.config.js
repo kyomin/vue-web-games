2dca19a8a31669e15bfe258b117ddb3ae123d727
kyomin <kim031504@naver.com>
2021-05-15 22:16:55 +0900

    숫자야구 게임 만들기 시작과 웹팩 기본 설정

---
+++ 숫자야구/webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    entry: {
+        app: './main.js' 
+    },
+    module: {
+        
+    },
+    plugins: [
+
+    ],
+    output: {
+        filename: 'app.js',
+        path: './dist'
+    }
+};
