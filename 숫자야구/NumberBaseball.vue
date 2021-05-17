ad631450d75cfed9b2bdc97abb890ee2f3407c86
kyomin <kim031504@naver.com>
2021-05-17 23:05:19 +0900

    숫자야구 게임에 v-for로 반복문 적용하기

--- 숫자야구/NumberBaseball.vue
+++ 숫자야구/NumberBaseball.vue
@@ -2,10 +2,16 @@
   <div>
       <h1>{{ result }}</h1>
       <form v-on:submit="onSubmitForm">
-          <input ref="answer" maxlength="4" v-model="value" type="text">
-          <button>입력</button>
+          <input ref="answer" maxlength="4" v-model="value" />
+          <button type="submit">입력</button>
       </form>
-      <div>시도: {{  }}</div>
+      <div>시도: {{ tries.length }}</div>
+      <ul>
+        <li v-for="t in tries">
+            <div> {{ t.try }} </div>
+            <div> {{ t.result }} </div>
+        </li>
+      </ul>
   </div>
 </template>
 
@@ -13,6 +19,7 @@
 export default {
     data() {
         return {
+            tries: [],
             value: '',
             result: ''
         }
@@ -20,6 +27,18 @@ export default {
     methods: {
         onSubmitForm(e) {
             e.preventDefault();
+            
+            if(this.value.length < 4) {
+                alert('4자리 숫자를 입력해 주십시오!');
+                return;
+            }
+
+            this.tries.push({
+                try: this.value,
+                result: '홈런'
+            });
+            this.value = '';
+            this.$refs.answer.focus();
         }
     }
 }
--- 숫자야구/main.js
+++ 숫자야구/main.js
@@ -1,9 +1,10 @@
 /* 
     웹팩의 entry 파일이다.
     웹팩이 이를 통해 
-    NumberBaseball.html + NumberBaseball.vue + Vue 인스턴스를 하나로 합쳐
-    
+    NumberBaseball.vue + Vue 인스턴스를 하나로 합쳐
     ./dist/app.js로 만들어주는 것이다.
+
+    이를 NumberBaseball.html의 root div에 주입한다.
 */
 import Vue from 'vue';
 import NumberBaseball from './NumberBaseball.vue';
