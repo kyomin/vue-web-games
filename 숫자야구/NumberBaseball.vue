edd1fe05bc8100d464f6d38ffce85e7f9c81054a
kyomin <kim031504@naver.com>
2021-05-17 23:40:22 +0900

    숫자야구 게임 완성

--- 숫자야구/NumberBaseball.vue
+++ 숫자야구/NumberBaseball.vue
@@ -16,9 +16,23 @@
 </template>
 
 <script>
+// 랜덤 숫자 4개를 뽑는다.
+const getNumbers = () => {
+    const candidates = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    const array = [];
+
+    for(let i=0; i<4; i++) {
+        const chosen = candidates.splice(Math.floor(Math.random() * (9-i)), 1)[0];
+        array.push(chosen);
+    }
+
+    return array;
+}
+
 export default {
     data() {
         return {
+            answer: getNumbers(),
             tries: [],
             value: '',
             result: ''
@@ -33,12 +47,46 @@ export default {
                 return;
             }
 
-            this.tries.push({
-                try: this.value,
-                result: '홈런'
-            });
-            this.value = '';
-            this.$refs.answer.focus();
+            if(this.value === this.answer.join('')) {   // 정답을 맞췄으면
+                this.tries.push({
+                    try: this.value,
+                    result: '홈런'
+                });
+                this.result = '홈런';
+                alert('정답입니다!\n 게임을 다시 실행합니다.');
+                this.value = '';
+                this.tries = [];
+                this.answer = getNumbers();
+                this.$refs.answer.focus();
+            } else {    // 정답 틀렸을 때
+                if(this.tries.length >= 9) {    // 10번째 시도
+                    this.result = `10번 넘게 틀려서 실패! 답은 ${this.answer.join(',')}였습니다.`;
+                    alert('게임을 다시 시작합니다.');
+                    this.value = '';
+                    this.answer = getNumbers();
+                    this.tries = [];
+                    this.$refs.answer.focus();
+                } else {
+                    let strike = 0;
+                    let ball = 0;
+                    const answerArray = this.value.split('').map(v => parseInt(v));
+
+                    for(let i=0; i<4; i++) {
+                        if(answerArray[i] === this.answer[i]) {     // 숫자, 자릿수 모두 맞았을 때
+                            strike++;
+                        } else if(this.answer.includes(answerArray[i])) {   // 숫자만 정답
+                            ball++;
+                        }
+                    }
+
+                    this.tries.push({
+                        try: this.value,
+                        result: `${strike} 스트라이크, ${ball} 볼입니다.`
+                    });
+                    this.value = '';
+                    this.$refs.answer.focus();
+                }
+            }
         }
     }
 }
