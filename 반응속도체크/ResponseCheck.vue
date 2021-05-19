f333c1bb968836c5554c829be4e4924dfea52a08
kyomin <kim031504@naver.com>
2021-05-19 14:30:14 +0900

    반응속도체크와 webpack watch 기능

---
+++ 반응속도체크/ResponseCheck.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div id="screen">{{ message }}</div>
+    <div>
+        <div>평균 시간: {{  }}</div>
+    </div>
+    <button v-on:click="onReset">리셋</button>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            result: [],
+            state: 'waiting',
+            message: '클릭해서 시작하세요'
+        }
+    },
+    methods: {
+        onReset() {
+
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ 반응속도체크/webpack.config.js
@@ -0,0 +1,32 @@
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    devtool: 'eval',
+    // import 시에 확장자 생략할 수 있게 된다.
+    resolve: {
+        extensions: ['.js', '.vue']
+    },
+    // 이 부분을 시작 지점으로 정해 처리하기 시작한다.
+    entry: {
+        app: './main.js'
+    },
+    // 웹팩은 기본적으로 js 파일들을 묶는다.
+    // 하지만, 타입스크립트 등 다른 확장자를 해석하기 위한 기준을 아래에서 마련해야 한다.
+    module: {
+        rules: [{
+            test: /\.vue$/,
+            loader: 'vue-loader'
+        }]
+    },
+    plugins: [
+        new VueLoaderPlugin()
+    ],
+    // 웹팩이 번들한 최종 결과물이 나온다.
+    // 이를 html에서 script에 명시만 하면 여러 js 파일들을 로드할 수 있는 효과를 얻게 된다.
+    output: {
+        filename: 'app.js',
+        path: path.join(__dirname, 'dist')
+    }
+};
