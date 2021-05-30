f5c22aca76764f6483078fbb779116c2d89d19bf
kyomin <kim031504@naver.com>
2021-05-30 22:54:41 +0900

    로또 추첨기 - 자식 컴포넌트에 props 적용

--- 로또/LottoBall.vue
+++ 로또/LottoBall.vue
@@ -1,13 +1,31 @@
 <template>
-  
+  <div class="ball" :style="{ background }">{{ number }}</div>
 </template>
 
 <script>
 export default {
+    name: 'LottoBall',  // 컴포넌트의 이름 지정. 지정하지 않으면 파일명이 디폴트로 컴포넌트명이 된다.
+    props: ['number'],
+    data() {
+        let background;
 
+        return {
+            background
+        };
+    }
 }
 </script>
 
 <style>
-
+    .ball {
+        display: inline-block;
+        border: 1px solid black;
+        border-radius: 20px;
+        width: 40px;
+        height: 40px;
+        line-height: 40px;
+        font-size: 20px;
+        text-align: center;
+        margin-right: 20px;
+    }
 </style>
--- 로또/LottoGenerator.vue
+++ 로또/LottoGenerator.vue
@@ -2,7 +2,7 @@
   <div>
       <div>당첨 숫자</div>
       <div id="결과창">
-          <lotto-ball v-for="ball in winBalls"></lotto-ball>
+          <lotto-ball v-for="ball in winBalls" number="5"></lotto-ball>
       </div>
       <div>보너스</div>
       <lotto-ball v-if="bonus"></lotto-ball>
@@ -11,9 +11,31 @@
 </template>
 
 <script>
+import LottoBall from './LottoBall';
+
+function getWinNumbers() {
+    console.log('getWinNumbers');
+
+    const candidate = Array(45).fill().map((v, i) => i + 1);
+    const shuffle = [];
+
+    while (candidate.length > 0) {
+        shuffle.push(candidate.splice(Math.floor(Math.random()*candidate.length), 1)[0]);
+    }
+
+    const bonusNumber = shuffle[shuffle.length-1];
+    const winNumbers = shuffle.slice(0, 6).sort((p, c) => p - c);
+
+    return [...winNumbers, bonusNumber];
+}
+
 export default {
+    components: {
+        'lotto-ball': LottoBall
+    },
     data() {
         return {
+            winNumbers: getWinNumbers(),
             winBalls: [],
             bonus: null,
             redo: false
