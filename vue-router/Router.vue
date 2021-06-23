6d4e9fcee023ab6699110fe393c9c4d930c885cb
kyomin <kim031504@naver.com>
2021-06-23 17:04:14 +0900

    Vue Router 셋팅하기

---
+++ vue-router/Router.vue
@@ -0,0 +1,16 @@
+<template>
+  
+</template>
+
+<script>
+import router from './routes';
+
+export default {
+    router,
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue-router/routes.js
@@ -0,0 +1,18 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import NumberBaseball from '../숫자야구/NumberBaseball';
+import ResponseCheck from '../반응속도체크/ResponseCheck';
+import RockScissorsPaper from '../가위바위보/RockScissorsPaper';
+import LottoGenerator from '../로또/LottoGenerator';
+
+// 뷰와 외부 라이브러리를 연결한다.
+Vue.use(VueRouter);
+
+export default new VueRouter({
+    routes: [
+        { path: '/number-baseball', component: NumberBaseball },
+        { path: '/response-check', component: ResponseCheck },
+        { path: '/rock-scissors-paper', component: RockScissorsPaper },
+        { path: '/lotto-generator', component: LottoGenerator },
+    ]
+});
